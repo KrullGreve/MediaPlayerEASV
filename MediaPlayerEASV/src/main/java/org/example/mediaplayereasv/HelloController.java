0f0bee3ee8fbd8a04a22b0fd8e7c81c1c7131ee9
TobiasFalck <TobiasFalck13@gmail.com>
2025-01-07 12:51:25 +0100

    Bug

--- MediaPlayerEASV/src/main/java/org/example/mediaplayereasv/HelloController.java
+++ MediaPlayerEASV/src/main/java/org/example/mediaplayereasv/HelloController.java
@@ -1,63 +1,23 @@
-package your.package.name;
+package org.example.mediaplayereasv;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.media.MediaView;
-import javafx.scene.input.MouseEvent;
-import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 
-public class HelloController{
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
 
     @FXML
-    private MediaView mvMainImage;
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 
-    @FXML
-    private Label myDuration;
-
-    @FXML
-    private Slider mySliderDuration;
-
-    @FXML
-    private Button bntShuffel;
-
-    @FXML
-    private Button bntStop;
-
-    @FXML
-    private Button bntBack;
-
-    @FXML
-    private Button bntPlay;
-
-    @FXML
-    private Button bntNext;
-
-    @FXML
-    private Button bntRepeat;
-
-    @FXML
-    private Button bntMute;
-
-    @FXML
-    private Slider mySliderVolume;
-
-    @FXML
-    private ListView<String> lvPlayList;
-
-    @FXML
-    private ListView<String> lvPlayLists;
-
-    @FXML
-    private ComboBox<String> cbSearchBar;
-
-    @FXML
-    private Label myLabelTitel;
-
-    @FXML
-    private Label myLabelCurrentSong;
-
-    @FXML
-    private ImageView ivIcon;
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        File mediaFolder = new File("src/main/Music");
+    }
 }
