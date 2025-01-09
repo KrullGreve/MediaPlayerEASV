6f9d637af20fccc5881f9df33fffee0e7456a0f8
Tozic <Mikcollatz@gmail.com>
2025-01-09 11:09:48 +0100

    Song Selecter

--- MediaPlayerEASV/src/main/java/org/example/mediaplayereasv/HelloController.java
+++ MediaPlayerEASV/src/main/java/org/example/mediaplayereasv/HelloController.java
@@ -1,18 +1,17 @@
 package org.example.mediaplayereasv;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.media.Media;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.media.MediaPlayer;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class HelloController {
-    private MediaPlayer mediaPlayer;
 
-    @FXML
-    private void onHelloButtonClick(ActionEvent event) {
-    }
+
+
 }
